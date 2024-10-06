1fa4312b7e61ba8f23ce36e0622b05f2288a8885
Sujayvignesh <sujayvignesh2005@gmail.com>
2024-10-06 20:58:42 +0530

    loop2.java

---
+++ loop2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SeriesGenerator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int q = scanner.nextInt();
+        for (int i = 0; i < q; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int n = scanner.nextInt();
+            
+            int sum = a;
+            for (int j = 0; j < n; j++) {
+                sum += (int)(Math.pow(2, j) * b);
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
+}
